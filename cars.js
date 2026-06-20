35b6d1dcc4a9c19387ff9ca02e1d4bb4d269c591
bekdan221 <bekdantemirbekov44@gmail.com>
2026-06-20 20:24:17 +0600

    Update cars.js

--- cars.js
+++ cars.js
@@ -1,31 +1,43 @@
-const cars = [
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Car View</title>
+<link rel="stylesheet" href="style.css">
+</head>
 
-/* BMW */
-{id:"bmw1",brand:"BMW",name:"BMW M5 Competition",price:"$65,000",img:"https://upload.wikimedia.org/wikipedia/commons/6/6b/2019_BMW_M5.jpg"},
-{id:"bmw2",brand:"BMW",name:"BMW M4",price:"$70,000",img:"https://upload.wikimedia.org/wikipedia/commons/4/44/BMW_M4.jpg"},
-{id:"bmw3",brand:"BMW",name:"BMW X5 M",price:"$85,000",img:"https://upload.wikimedia.org/wikipedia/commons/0/0c/BMW_X5_M.jpg"},
-{id:"bmw4",brand:"BMW",name:"BMW M3",price:"$60,000",img:"https://upload.wikimedia.org/wikipedia/commons/0/0d/BMW_M3.jpg"},
-{id:"bmw5",brand:"BMW",name:"BMW M8",price:"$120,000",img:"https://upload.wikimedia.org/wikipedia/commons/1/1c/BMW_M8.jpg"},
-{id:"bmw6",brand:"BMW",name:"BMW X6",price:"$75,000",img:"https://upload.wikimedia.org/wikipedia/commons/3/3c/BMW_X6.jpg"},
+<body>
 
-/* Audi */
-{id:"a1",brand:"Audi",name:"Audi RS6",price:"$90,000",img:"https://upload.wikimedia.org/audi_rs6.jpg"},
-{id:"a2",brand:"Audi",name:"Audi RS7",price:"$110,000",img:"https://upload.wikimedia.org/audi_rs7.jpg"},
-{id:"a3",brand:"Audi",name:"Audi A8",price:"$85,000",img:"https://upload.wikimedia.org/audi_a8.jpg"},
+<header>🚗 CAR DETAILS</header>
 
-/* Mercedes */
-{id:"m1",brand:"Mercedes",name:"AMG GT",price:"$130,000",img:"https://upload.wikimedia.org/amg_gt.jpg"},
-{id:"m2",brand:"Mercedes",name:"E63 AMG",price:"$88,000",img:"https://upload.wikimedia.org/e63.jpg"},
-{id:"m3",brand:"Mercedes",name:"S Class",price:"$120,000",img:"https://upload.wikimedia.org/sclass.jpg"},
+<div class="carView">
 
-/* Toyota */
-{id:"t1",brand:"Toyota",name:"Camry SE",price:"$28,000",img:"https://upload.wikimedia.org/camry.jpg"},
-{id:"t2",brand:"Toyota",name:"Camry XSE",price:"$30,000",img:"https://upload.wikimedia.org/camry2.jpg"},
-{id:"t3",brand:"Toyota",name:"Supra",price:"$55,000",img:"https://upload.wikimedia.org/supra.jpg"},
-{id:"t4",brand:"Toyota",name:"Land Cruiser",price:"$85,000",img:"https://upload.wikimedia.org/landcruiser.jpg"},
+<img id="img" class="mainImg">
 
-/* Lexus */
-{id:"l1",brand:"Lexus",name:"LX 600",price:"$95,000",img:"https://upload.wikimedia.org/lx600.jpg"},
-{id:"l2",brand:"Lexus",name:"RX 500h",price:"$65,000",img:"https://upload.wikimedia.org/rx.jpg"}
+<div id="info" class="info"></div>
 
-];
+</div>
+
+<script src="cars.js"></script>
+
+<script>
+const id=new URLSearchParams(location.search).get("id");
+const car=cars.find(c=>c.id===id);
+
+document.getElementById("img").src=car.img;
+
+document.getElementById("info").innerHTML=`
+<h2>${car.name}</h2>
+<p>💰 Price: $${car.price}</p>
+<p>📊 Mileage: ${car.mileage} km</p>
+<p>📅 Year: ${car.year}</p>
+<p>⚙ Engine: ${car.engine}</p>
+<p>🔧 Transmission: ${car.transmission}</p>
+<p>📍 ${car.country}, ${car.city}</p>
+<p>🧾 Condition: ${car.condition}</p>
+`;
+
+</script>
+
+</body>
+</html>
