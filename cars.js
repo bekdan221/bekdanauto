828b9255b253463df1d7227640fd99fcdabb9c6d
bekdan221 <bekdantemirbekov44@gmail.com>
2026-06-20 19:58:51 +0600

    Update cars.js

--- cars.js
+++ cars.js
@@ -1,25 +1,31 @@
 const cars = [
-  {id:"bmw-m5",brand:"BMW",name:"BMW M5",price:"$65,000",mileage:"58,000 km",
-  images:["https://upload.wikimedia.org/wikipedia/commons/6/6b/2019_BMW_M5.jpg"]},
 
-  {id:"bmw-x5",brand:"BMW",name:"BMW X5",price:"$80,000",mileage:"40,000 km",
-  images:["https://upload.wikimedia.org/wikipedia/commons/0/0c/BMW_X5.jpg"]},
+/* BMW */
+{id:"bmw1",brand:"BMW",name:"BMW M5 Competition",price:"$65,000",img:"https://upload.wikimedia.org/wikipedia/commons/6/6b/2019_BMW_M5.jpg"},
+{id:"bmw2",brand:"BMW",name:"BMW M4",price:"$70,000",img:"https://upload.wikimedia.org/wikipedia/commons/4/44/BMW_M4.jpg"},
+{id:"bmw3",brand:"BMW",name:"BMW X5 M",price:"$85,000",img:"https://upload.wikimedia.org/wikipedia/commons/0/0c/BMW_X5_M.jpg"},
+{id:"bmw4",brand:"BMW",name:"BMW M3",price:"$60,000",img:"https://upload.wikimedia.org/wikipedia/commons/0/0d/BMW_M3.jpg"},
+{id:"bmw5",brand:"BMW",name:"BMW M8",price:"$120,000",img:"https://upload.wikimedia.org/wikipedia/commons/1/1c/BMW_M8.jpg"},
+{id:"bmw6",brand:"BMW",name:"BMW X6",price:"$75,000",img:"https://upload.wikimedia.org/wikipedia/commons/3/3c/BMW_X6.jpg"},
 
-  {id:"audi-rs6",brand:"Audi",name:"Audi RS6",price:"$90,000",mileage:"35,000 km",
-  images:["https://upload.wikimedia.org/wikipedia/commons/4/4e/Audi_RS6.jpg"]},
+/* Audi */
+{id:"a1",brand:"Audi",name:"Audi RS6",price:"$90,000",img:"https://upload.wikimedia.org/audi_rs6.jpg"},
+{id:"a2",brand:"Audi",name:"Audi RS7",price:"$110,000",img:"https://upload.wikimedia.org/audi_rs7.jpg"},
+{id:"a3",brand:"Audi",name:"Audi A8",price:"$85,000",img:"https://upload.wikimedia.org/audi_a8.jpg"},
 
-  {id:"merc-e63",brand:"Mercedes",name:"Mercedes E63 AMG",price:"$88,000",mileage:"50,000 km",
-  images:["https://upload.wikimedia.org/wikipedia/commons/3/3f/Mercedes_AMG_E63.jpg"]},
+/* Mercedes */
+{id:"m1",brand:"Mercedes",name:"AMG GT",price:"$130,000",img:"https://upload.wikimedia.org/amg_gt.jpg"},
+{id:"m2",brand:"Mercedes",name:"E63 AMG",price:"$88,000",img:"https://upload.wikimedia.org/e63.jpg"},
+{id:"m3",brand:"Mercedes",name:"S Class",price:"$120,000",img:"https://upload.wikimedia.org/sclass.jpg"},
 
-  {id:"toyota-camry",brand:"Toyota",name:"Toyota Camry",price:"$28,000",mileage:"42,000 km",
-  images:["https://upload.wikimedia.org/wikipedia/commons/9/9b/Toyota_Camry.jpg"]},
+/* Toyota */
+{id:"t1",brand:"Toyota",name:"Camry SE",price:"$28,000",img:"https://upload.wikimedia.org/camry.jpg"},
+{id:"t2",brand:"Toyota",name:"Camry XSE",price:"$30,000",img:"https://upload.wikimedia.org/camry2.jpg"},
+{id:"t3",brand:"Toyota",name:"Supra",price:"$55,000",img:"https://upload.wikimedia.org/supra.jpg"},
+{id:"t4",brand:"Toyota",name:"Land Cruiser",price:"$85,000",img:"https://upload.wikimedia.org/landcruiser.jpg"},
 
-  {id:"lexus-lx",brand:"Lexus",name:"Lexus LX 600",price:"$95,000",mileage:"18,000 km",
-  images:["https://upload.wikimedia.org/wikipedia/commons/3/3f/Lexus_LX.jpg"]},
+/* Lexus */
+{id:"l1",brand:"Lexus",name:"LX 600",price:"$95,000",img:"https://upload.wikimedia.org/lx600.jpg"},
+{id:"l2",brand:"Lexus",name:"RX 500h",price:"$65,000",img:"https://upload.wikimedia.org/rx.jpg"}
 
-  {id:"porsche-911",brand:"Porsche",name:"Porsche 911",price:"$180,000",mileage:"12,000 km",
-  images:["https://upload.wikimedia.org/wikipedia/commons/7/7e/Porsche_911.jpg"]},
-
-  {id:"range-rover",brand:"Range Rover",name:"Range Rover Sport",price:"$110,000",mileage:"25,000 km",
-  images:["https://upload.wikimedia.org/wikipedia/commons/1/1b/Range_Rover.jpg"]}
 ];
