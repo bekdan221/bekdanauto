80b4f269e6f018e28272fc69b9512362707d7c9c
bekdan221 <bekdantemirbekov44@gmail.com>
2026-06-20 20:29:18 +0600

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,1 +1,15 @@
-console.log("BekdanAuto loaded");
+document.addEventListener("DOMContentLoaded",()=>{
+
+document.querySelectorAll(".card").forEach((el,i)=>{
+el.style.opacity=0;
+el.style.transform="translateY(20px)";
+
+setTimeout(()=>{
+el.style.transition="0.4s";
+el.style.opacity=1;
+el.style.transform="translateY(0)";
+},i*80);
+
+});
+
+});
