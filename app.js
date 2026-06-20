1a521c5d4af11a3e66bae146dfe9d9d1a4bb08b5
bekdan221 <bekdantemirbekov44@gmail.com>
2026-06-20 20:42:07 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,61 @@
-function render(list){
-const c=document.getElementById("catalog");
-c.innerHTML="";
+let quiz = [];
+let index = 0;
+let score = 0;
 
-list.forEach(car=>{
-c.innerHTML+=`
-<div class="card" onclick="openCar('${car.id}')">
-<img src="${car.img}">
-<h3>${car.name}</h3>
-<p>$${car.price}</p>
-</div>
-`;
-});
+function startQuiz(n){
+quiz = quizzes[n];
+index = 0;
+score = 0;
+load();
 }
 
-render(cars);
+function load(){
 
-function openCar(id){
-location.href="car.html?id="+id;
+let q = quiz[index];
+
+document.getElementById("question").textContent = q.q;
+
+let answers = document.getElementById("answers");
+answers.innerHTML = "";
+
+q.a.forEach((ans,i)=>{
+let b = document.createElement("button");
+b.textContent = ans;
+
+b.onclick = ()=>{
+if(i === q.c) score++;
+document.querySelectorAll("button").forEach(x=>x.disabled=true);
+updateScore();
+};
+
+answers.appendChild(b);
+});
+
+updateProgress();
+updateScore();
+}
+
+function next(){
+index++;
+
+if(index >= quiz.length){
+document.getElementById("question").textContent =
+"Тест завершён!";
+
+document.getElementById("answers").innerHTML = "";
+
+return;
+}
+
+load();
+}
+
+function updateScore(){
+document.getElementById("score").textContent =
+`Score: ${score} / ${quiz.length}`;
+}
+
+function updateProgress(){
+let p = (index / quiz.length) * 100;
+document.getElementById("progress-bar").style.width = p + "%";
 }
